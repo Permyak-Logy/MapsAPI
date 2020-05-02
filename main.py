5c35d852730d539d7169c4cf195b88a40ed9801c
Permyak-Logy <maxsika2004@mail.ru>
2020-05-02 12:51:44 +0400

    task 7

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from PyQt5.QtWidgets import QApplication, QMainWindow, QLabel, QComboBox, QPushButton
+from PyQt5.QtWidgets import QApplication, QMainWindow, QLabel, QComboBox, QPushButton, QLineEdit, QRadioButton
 from PyQt5.QtGui import QPixmap
 from PyQt5.Qt import Qt
 import requests
@@ -8,6 +8,50 @@ import os
 SCREEN_SIZE = [600, 450]
 
 
+class GeocoderAPI:
+    _server = 'http://geocode-maps.yandex.ru/1.x/'
+    __apikey = '40d1649f-0493-4b70-98ba-98533de7710b'
+
+    @staticmethod
+    def get_toponym(toponym_to_find, apikey=None, api_server=None):
+        geocoder_params = {
+            "apikey": GeocoderAPI.__apikey,
+            "geocode": toponym_to_find,
+            "format": "json"}
+
+        response = requests.get(GeocoderAPI._server, params=geocoder_params)
+        if response is None:
+            raise Exception(response.reason)
+        json_response = response.json()
+        toponym = json_response["response"]["GeoObjectCollection"][
+            "featureMember"][0]["GeoObject"]
+        return toponym
+
+    @staticmethod
+    def get_spn(toponym_to_find=None, cornera=None, cornerb=None) -> list:
+        if toponym_to_find is not None:
+            toponym = GeocoderAPI.get_toponym(toponym_to_find)
+            toponym_enevelope = toponym["boundedBy"]["Envelope"]
+            toponym_lower_corner = list(map(float, toponym_enevelope["lowerCorner"].split()))
+            toponym_upper_corner = list(map(float, toponym_enevelope["upperCorner"].split()))
+            return [abs(toponym_upper_corner[0] - toponym_lower_corner[0]),
+                    abs(toponym_upper_corner[1] - toponym_lower_corner[1])]
+        else:
+            toponym_lower_corner = list(map(float, cornera.split(',')))
+            toponym_upper_corner = list(map(float, cornerb.split(',')))
+            return [1.9 * abs(toponym_upper_corner[0] - toponym_lower_corner[0]),
+                    1.9 * abs(toponym_upper_corner[1] - toponym_lower_corner[1])]
+
+    @staticmethod
+    def get_coordinates(toponym_to_find) -> str:
+        toponym = GeocoderAPI.get_toponym(toponym_to_find)
+        return toponym["Point"]["pos"]
+
+    @staticmethod
+    def get_longitude_and_lattitude(toponym_to_find) -> list:
+        return list(map(float, GeocoderAPI.get_coordinates(toponym_to_find).split(" ")))
+
+
 class StaticMapAPI:
     map_file = 'map.png'
     map_file_for_sat = 'map.jpg'
@@ -15,24 +59,28 @@ class StaticMapAPI:
     ll = [37.620070, 55.753630]
     size = [600, 450]
     l = 'map'
-    spn = 0.002
+    spn = [0.002, 0.002]
     MAX_SPN = 65.536
     MIN_SPN = 0.001
     pixmap = None
+    pt = ''
 
     def set_params(self, **params):
         self.l = params.get('l', self.l)
         self.spn = params.get('spn', self.spn)
         self.size = params.get('size', self.size)
         self.ll = params.get('ll', self.ll)
+        self.pt = params.get('pt', self.pt)
 
     def update_map(self):
         params = {
             'll': f'{self.ll[0]},{self.ll[1]}',
             'size': f'{self.size[0]},{self.size[1]}',
             'l': self.l,
-            'spn': f'{self.spn},{self.spn}'
+            'spn': f'{self.spn[0]},{self.spn[1]}'
         }
+        if self.pt:
+            params['pt'] = self.pt
         response = requests.get(self._server, params=params)
         if not response:
             raise Exception(response.reason)
@@ -55,18 +103,24 @@ class StaticMapAPI:
         return self.size
 
     def spn_in(self):
-        self.spn /= 2
-        if self.spn < self.MIN_SPN:
-            self.spn = self.MIN_SPN
+        self.spn[0] /= 2
+        self.spn[1] /= 2
+        if self.spn[0] < self.MIN_SPN:
+            self.spn[0] = self.MIN_SPN
+        if self.spn[1] < self.MIN_SPN:
+            self.spn[1] = self.MIN_SPN
 
     def spn_out(self):
-        self.spn *= 2
-        if self.spn > self.MAX_SPN:
-            self.spn = self.MAX_SPN
+        self.spn[0] *= 2
+        self.spn[1] *= 2
+        if self.spn[0] > self.MAX_SPN:
+            self.spn[0] = self.MAX_SPN
+        if self.spn[1] > self.MAX_SPN:
+            self.spn[1] = self.MAX_SPN
 
     def move(self, x=0, y=0):
-        self.ll[0] += x * self.spn * 1 * 2.5
-        self.ll[1] += y * self.spn * (85 / 180) * 2.5
+        self.ll[0] += x * self.spn[0]
+        self.ll[1] += y * self.spn[1]
 
 
 class MapsAPI(QMainWindow):
@@ -75,24 +129,49 @@ class MapsAPI(QMainWindow):
         self.static_map_api = StaticMapAPI()
         self.initUI()
 
-        self.btn_update_map.clicked.connect(self.update_map)
-        # self.mode.currentTextChanged.connect(self.update_map)
+        [btn.clicked.connect(self.update_map) for btn in self.mode_buttons]
+        self.btn_search_obj.clicked.connect(self.search_obj)
+        self.btn_discharge_obj.clicked.connect(self.discharge_search)
 
     def initUI(self):
         self.setWindowTitle('Maps API')
         self.setFixedSize(*SCREEN_SIZE)
         self.label_map = QLabel(self)
         self.label_map.move(100, 0)
-        self.mode = QComboBox(self)
-        self.mode.addItems(['map', 'sat', 'skl'])
-        self.btn_update_map = QPushButton('Обновить', self)
-        self.btn_update_map.move(0, 100)
+        self.mode_buttons = [QRadioButton(text, self) for text in ['map', 'sat', 'sat,skl']]
+        self.mode_buttons[0].setChecked(True)
+        [elem.move(10, 10 + 30 * i) for i, elem in enumerate(self.mode_buttons)]
+
+        self.line_edit_search = QLineEdit(self)
+        self.line_edit_search.move(0, 100)
+
+        self.btn_search_obj = QPushButton('Искать', self)
+        self.btn_search_obj.move(0, 130)
+
+        self.btn_discharge_obj = QPushButton('Сброс', self)
+        self.btn_discharge_obj.move(0, 160)
+
         self.update_map()
 
     def update_map(self):
-        self.static_map_api.set_params(l=self.mode.currentText())
+        self.static_map_api.set_params(l=[btn.text() for btn in self.mode_buttons if btn.isChecked()][0])
         self.label_map.setPixmap(self.static_map_api.get_pixmap(update=True))
         self.label_map.resize(*self.static_map_api.get_size())
+
+    def search_obj(self):
+        try:
+            toponym_to_find = self.line_edit_search.text()
+            ll = GeocoderAPI.get_longitude_and_lattitude(toponym_to_find)
+            spn = GeocoderAPI.get_spn(toponym_to_find)
+            self.static_map_api.set_params(ll=ll, pt=f'{ll[0]},{ll[1]}', spn=spn)
+            self.update_map()
+        except IndexError as E:
+            print('Объект не найден')
+
+    def discharge_search(self):
+        self.static_map_api.set_params(pt=None)
+        self.line_edit_search.setText('')
+        self.update_map()
 
     def keyPressEvent(self, event):
         if event.key() == Qt.Key_PageUp:
